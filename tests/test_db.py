0da6d28c4758e9d0cb669157a0b9efdc3be47b73
Sebastian Nilsson <seblerr@gmail.com>
2025-09-09 13:08:33 +0200

    test: new tests for sorting and week period
    also refactors test to avoid copy paste

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,33 +7,41 @@ class TestDatabase(unittest.TestCase):
     def setUp(self):
         self.conn = sqlite3.connect(":memory:")
         self.db = Database(conn=self.conn)
+        self._create_tables()
+        self._add_game_with_scores(
+            "game_id",
+            [
+                ("player1", 1, 3000),
+                ("player1", 2, 2000),
+                ("player2", 1, 1000),
+                ("player2", 2, 2000),
+            ],
+        )
+        self._add_game_with_scores(
+            "game_id2",
+            [
+                ("player1", 1, 2000),
+                ("player1", 2, 3000),
+                ("player2", 1, 2000),
+                ("player2", 2, 4000),
+            ],
+        )
+        self._add_game_with_scores(
+            "game_id3",
+            [
+                ("player1", 1, 2000),
+                ("player1", 2, 3000),
+                ("player2", 1, 5000),
+                ("player2", 2, 2000),
+            ],
+        )
 
     def tearDown(self):
         self.conn.close()
 
-    def test_add_and_get_game(self):
-        """Test that a game can be added and retrieved."""
+    def _create_tables(self):
         with self.db.db_connection() as conn:
             cursor = conn.cursor()
-            # Manually create tables in the in-memory database
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS games (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                game_id TEXT UNIQUE,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-            """)
-            conn.commit()
-
-        self.db.add_game("game_id")
-        game_id = self.db.get_latest_game_id()
-        self.assertEqual(game_id, "game_id")
-
-    def test_add_scores(self):
-        """Test that scores can be added for a game."""
-        with self.db.db_connection() as conn:
-            cursor = conn.cursor()
-            # Manually create tables in the in-memory database
             cursor.execute("""
             CREATE TABLE IF NOT EXISTS games (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -54,62 +62,70 @@ class TestDatabase(unittest.TestCase):
             """)
             conn.commit()
 
-        self.db.add_game("game_id")
+    def _add_game_with_scores(self, game_id: str, scoresheet: list[tuple]):
+        self.db.add_game(game_id)
+        self.db.add_scores(game_id, scoresheet)
 
-        scoresheet = [
-            ("player1", 1, 5000),
-            ("player1", 2, 3000),
-            ("player2", 1, 4500),
-        ]
-        self.db.add_scores("game_id", scoresheet)
+    def test_add_and_get_game(self):
+        """Test that a game can be added and retrieved."""
+        self.db.add_game("unique_game_id")
+        game_id = self.db.get_latest_game_id()
+        self.assertEqual(game_id, "unique_game_id")
+
+    def test_add_scores(self):
+        """Test that scores can be added for a game."""
 
         with self.db.db_connection() as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM scores")
             scores = cursor.fetchall()
-            self.assertEqual(len(scores), 3)
+            self.assertEqual(len(scores), 12)
             self.assertEqual(scores[0][2], "player1")
-            self.assertEqual(scores[0][4], 5000)
+            self.assertEqual(scores[0][4], 3000)
 
-    def test_get_scores_sorts_correctly(self):
-        """Test that get_scores correctly sorts players by total score."""
-        with self.db.db_connection() as conn:
-            cursor = conn.cursor()
-            # Manually create tables in the in-memory database
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS games (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                game_id TEXT UNIQUE,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-            """)
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS scores (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                game_id TEXT,
-                player_name TEXT,
-                round_number INTEGER,
-                score INTEGER,
-                UNIQUE(game_id, player_name, round_number),
-                FOREIGN KEY (game_id) REFERENCES games(game_id)
-            )
-            """)
-            conn.commit()
+    def test_get_scores_from_game(self):
+        """Test that get_scores can fetch score from specific game"""
 
-        game_id = "game_id"
-        self.db.add_game(game_id)
+        scores = self.db.get_scores("game_id", None, False)
 
-        player1_scores = [
-            ("player1", 1, 1000),
-            ("player1", 2, 2000),
-        ]
-        player2_scores = [
-            ("player2", 1, 5000),
-            ("player2", 2, 4500),
-        ]
-        self.db.add_scores(game_id, player1_scores + player2_scores)
+        player1_position = scores.find("player1")
+        player2_position = scores.find("player2")
 
-        scores = self.db.get_scores()
+        self.assertTrue(
+            player1_position < player2_position,
+            "Player1 should be listed before Player2",
+        )
+
+    def test_get_scores_sort_by_total(self):
+        """Test that get_scores correctly sorts players by total score (default)."""
+
+        scores = self.db.get_scores(None, None, False)
+
+        player1_position = scores.find("player1")
+        player2_position = scores.find("player2")
+
+        self.assertTrue(
+            player2_position < player1_position,
+            "Player2 should be listed before Player1",
+        )
+
+    def test_get_scores_sort_by_avg(self):
+        """Get scores and sort by avg."""
+
+        scores = self.db.get_scores(None, None, True)
+
+        player1_position = scores.find("player1")
+        player2_position = scores.find("player2")
+
+        self.assertTrue(
+            player2_position < player1_position,
+            "Player2 should be listed before Player1",
+        )
+
+    def test_get_week_scores_sorted_by_avg(self):
+        """Get scores by week and sort by avg."""
+
+        scores = self.db.get_scores(None, "week", True)
 
         player1_position = scores.find("player1")
         player2_position = scores.find("player2")
